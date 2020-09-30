234ab2d7a5fcfad767126a26ca5173cf2c52dd40
Kamil Bania <kamilbania2@gmail.com>
2020-09-30 19:41:31 +0200

    WindArea script and WindArea prefabs

---
+++ Assets/Scripts/Wind/WindArea.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindArea : MonoBehaviour
+{
+    ParticleSystem windParticleSystem;
+    AreaEffector2D windEffector;
+    BoxCollider2D boxCollider2D;
+
+    // Start is called before the first frame update
+    private void Awake() {
+        windEffector = GetComponent<AreaEffector2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        ParticleSystem.ShapeModule ps = windParticleSystem.shape;
+        ps.position = Vector3.zero - new Vector3(0, boxCollider2D.size.y / 2, 0);
+        ps.scale = new Vector3(boxCollider2D.size.x, boxCollider2D.size.y, 1);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
