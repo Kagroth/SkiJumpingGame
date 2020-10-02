3dfe76735742e19e3458c3c3ac5c18f39ceac23f
Kamil Bania <kamilbania2@gmail.com>
2020-10-03 01:23:30 +0200

    Fall state

--- Assets/Scripts/SkiJumperStates/FallState.cs
+++ Assets/Scripts/SkiJumperStates/FallState.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class FallState : SkiJumperState
 {
+    private bool enterFallState;
+
     public FallState(GameObject playerGameObjectToSet, StateMachine playerStateMachineToSet) : base(playerGameObjectToSet, playerStateMachineToSet) {
 
     }
     public override void Init() {
-
+        enterFallState = true;
     }
 
     public override void PhysicsUpdate() {
-        
+        if (enterFallState) {
+            playerRb.AddForce(Vector3.up * 5, ForceMode2D.Impulse);
+            playerRb.angularVelocity = 0;
+            enterFallState = false;
+        }
     }
 }
