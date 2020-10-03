fd4300cdf3219af5f3d144e8bcb92eb63ec0b834
Kamil Bania <kamilbania2@gmail.com>
2020-10-04 01:12:25 +0200

    LandingData class

---
+++ Assets/Scripts/SkiJumperStates/LandingData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandingData
+{
+    public const string TELEMARK = "Telemark";
+    public const string BOTH_LEGS = "BothLegs";
+
+    private string currentLanding; 
+    private float distanceToLandingSlope;
+    private Vector3 landingPoint;
+
+    public LandingData(string currLanding) {
+        currentLanding = currLanding;
+    }
+
+    public string GetLandingType() {
+        return currentLanding;
+    } 
+
+    public float GetDistance() {
+        return distanceToLandingSlope;
+    }
+
+    public Vector3 GetLandingPoint() {
+        return landingPoint;
+    }
+
+    public void SetDistanceToLandingSlope(float distance) {
+        distanceToLandingSlope = distance;
+    }
+
+    public void SetLandingPoint(Vector3 landingP) {
+        landingPoint = landingP;
+    }
+}
