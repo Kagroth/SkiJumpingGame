5a7c51dd6f03616a36ef7d52e1d0135b2852663e
Kamil Bania <kamilbania2@gmail.com>
2020-10-06 00:58:19 +0200

    Choose hill menu

---
+++ Assets/Scripts/ChooseHillMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChooseHillMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject attachedHill;
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        GetComponentInChildren<Text>().color = Color.white;
+    }
+    public void OnPointerExit(PointerEventData eventData) {
+        GetComponentInChildren<Text>().color = Color.black;
+    }
+
+    public void LoadHill() {
+        GameManager.hillPrefab = attachedHill;
+        SceneManager.LoadScene("Competition");
+    }
+}
