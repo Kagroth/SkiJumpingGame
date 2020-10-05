2203d61eee9d1af9639c6b4f0208cc10f0088bd6
Kamil Bania <kamilbania2@gmail.com>
2020-10-06 00:59:16 +0200

    UI Manager component

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject gameplayPanel;
+    
+    public Text hillInfo;
+    public Text lastScore;
+    public Text bestScore;
+    public Text landingType;
+
+    public GameObject helpPanel;
+
+    private bool helpPanelShow = false;
+
+    private void Awake() {
+        helpPanel.SetActive(helpPanelShow);    
+    }
+
+    public void ToggleHelpPanel() {
+        helpPanelShow = !helpPanelShow;
+        helpPanel.SetActive(helpPanelShow);
+        gameplayPanel.SetActive(!helpPanelShow);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
