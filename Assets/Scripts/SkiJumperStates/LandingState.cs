d57dc0379d4503c2be14ce8daa21cbbb452e4783
Kamil Bania <kamilbania2@gmail.com>
2020-10-12 14:48:11 +0200

    Animation transition from flying to landing, formatting file

--- Assets/Scripts/SkiJumperStates/LandingState.cs
+++ Assets/Scripts/SkiJumperStates/LandingState.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class LandingState : SkiJumperState
 {
-    public LandingState(GameObject playerGameObject, StateMachine playerStateMachine) : base(playerGameObject, playerStateMachine) {
+    public LandingState(GameObject playerGameObject, StateMachine playerStateMachine) : base(playerGameObject, playerStateMachine)
+    {
 
     }
 
-    public override void Init() {
+    public override void Init()
+    {
         Debug.Log("Podchodze do ladowania");
 
         RaycastHit2D[] hits2D = Physics2D.RaycastAll(playerGameObject.transform.position, Vector2.down, 1000);
         float hitDistance = 0;
 
-        foreach (RaycastHit2D hit in hits2D) {
-            if (hit.collider.gameObject.tag.Equals("LandingSlope")) {
+        foreach (RaycastHit2D hit in hits2D)
+        {
+            if (hit.collider.gameObject.tag.Equals("LandingSlope"))
+            {
                 hitDistance = hit.distance;
                 break;
             }
@@ -25,26 +29,45 @@ public class LandingState : SkiJumperState
         Debug.Log("Wysokość podejscia do ladowania: " + hitDistance);
     }
 
-    public override void HandleUpdate() {
+    public override void HandleUpdate()
+    {
+        if (Mathf.Abs(playerController.GetPelvisBone().localEulerAngles.z - 340) > 1f)
+        {
+            Transform pelvisBone = playerController.GetPelvisBone();
+            pelvisBone.localRotation = Quaternion.RotateTowards(pelvisBone.localRotation, Quaternion.Euler(0, 0, 340), Time.deltaTime * 100);
+        }
 
+        if (Mathf.Abs(playerController.GetFeetBone().localEulerAngles.z - 90) > 1f)
+        {
+            Transform feetBone = playerController.GetFeetBone();
+            feetBone.localRotation = Quaternion.RotateTowards(feetBone.localRotation, Quaternion.Euler(0, 0, 90), Time.deltaTime * 100);
+        }
+
+        if (playerGameObject.transform.localEulerAngles.z > 1f)
+        {
+            playerGameObject.transform.localRotation = Quaternion.RotateTowards(playerGameObject.transform.localRotation, Quaternion.Euler(0, 0, 0), Time.deltaTime * 100);
+        }
     }
 
-    public override void PhysicsUpdate() {
+    public override void PhysicsUpdate()
+    {
 
     }
 
     public override void HandleLanding()
     {
         LandingData landingData = playerStateMachine.GetLandingData();
-        
+
         float minHeight = 0;
         float minAngle = 0;
 
-        if (landingData.GetLandingType().Equals(LandingData.BOTH_LEGS)) {
+        if (landingData.GetLandingType().Equals(LandingData.BOTH_LEGS))
+        {
             minHeight = LandingData.BOTH_LEGS_MIN_HEIGTH;
             minAngle = LandingData.BOTH_LEGS_MIN_ANGLE;
         }
-        else if (landingData.GetLandingType().Equals(LandingData.TELEMARK)) {
+        else if (landingData.GetLandingType().Equals(LandingData.TELEMARK))
+        {
             minHeight = LandingData.TELEMARK_MIN_HEIGTH;
             minAngle = LandingData.TELEMARK_MIN_ANGLE;
         }
@@ -56,10 +79,12 @@ public class LandingState : SkiJumperState
 
         SkiJumperState landingResultState = null;
 
-        if (landingData.GetDistance() > minHeight) {
+        if (landingData.GetDistance() > minHeight)
+        {
             landingResultState = playerController.landedState;
         }
-        else {
+        else
+        {
             landingResultState = playerController.fallState;
         }
 
