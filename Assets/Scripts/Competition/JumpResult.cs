b8ec8cd67fdda5990ab45ed38b8853548aa11ed6
Kamil Bania <kamilbania2@gmail.com>
2020-10-30 20:10:13 +0100

    JumpResult class

---
+++ Assets/Scripts/Competition/JumpResult.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpResult
+{
+    [SerializeField]
+    public float jumpDistance;
+
+    [SerializeField]
+    public Judge[] judges;
+
+    [SerializeField]
+    public float jumpPoints;
+
+    public JumpResult(float distance, Judge[] judgesArr, float points)
+    {
+        jumpDistance = distance;
+        judges = judgesArr;
+        jumpPoints = points;
+    }
+}
