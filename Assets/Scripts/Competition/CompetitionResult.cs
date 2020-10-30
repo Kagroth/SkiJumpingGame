079d3d85eec720e07d8e04832e018858baa9453c
Kamil Bania <kamilbania2@gmail.com>
2020-10-30 20:19:01 +0100

    CompetitionResult - UI prefab

---
+++ Assets/Scripts/Competition/CompetitionResult.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompetitionResult
+{
+    public int position;
+    public SkiJumper skiJumper;
+    public JumpResult firstJumpResult;
+    public JumpResult secondJumpResult;
+    public float points;
+
+    public CompetitionResult() {
+
+    }
+
+    public CompetitionResult(int pos, SkiJumper jumperToSet) {
+        position = pos;
+        skiJumper = jumperToSet;
+        firstJumpResult = null;
+        secondJumpResult = null;
+        points = 0;
+    }
+}
