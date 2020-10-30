9374aa5dc695c2c21d540a73ff39472f8ef45bc4
Kamil Bania <kamilbania2@gmail.com>
2020-10-30 20:12:39 +0100

    SkiJumper class

---
+++ Assets/Scripts/SkiJumpersData/SkiJumper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkiJumper : MonoBehaviour
+{
+    public string skiJumperName;
+    public string country;
+    public bool isComputer;
+
+    public SkiJumper(string name, string countryToSet, bool computer) {
+        skiJumperName = name;
+        country = countryToSet;
+        isComputer = computer;
+    }
+}
