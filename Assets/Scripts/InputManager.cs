ba4cc7357a899f91c21b117cd162cd78534ccb2c
Kamil Bania <kamilbania2@gmail.com>
2020-10-31 01:30:04 +0100

    InputManager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static string currentInputMode;
+
+    public const string SKI_JUMPER = "SKI_JUMPER";
+    public const string JUMP_FINISHED = "JUMP_FINISHED";
+
+    public const string COMPETITION_UI = "COMPETITION_UI";
+
+    public static void SetInputMode(string inputMode) {
+        currentInputMode = inputMode;
+        Debug.Log("Zmieniam sterowanie na: " + inputMode);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
