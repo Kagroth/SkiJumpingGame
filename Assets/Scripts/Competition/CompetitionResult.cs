aa23868fc8aa1bedfcb7d1f580f78fcd63254b61
Kamil Bania <kamilbania2@gmail.com>
2020-10-31 01:31:40 +0100

    CompetitionResult jump results changed to List

--- Assets/Scripts/Competition/CompetitionResult.cs
+++ Assets/Scripts/Competition/CompetitionResult.cs
@@ -1,24 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 public class CompetitionResult
 {
     public int position;
     public SkiJumper skiJumper;
-    public JumpResult firstJumpResult;
-    public JumpResult secondJumpResult;
+    public List<JumpResult> jumpResults;
     public float points;
+
+    public int seriesCount;
 
     public CompetitionResult() {
 
     }
 
-    public CompetitionResult(int pos, SkiJumper jumperToSet) {
+    public CompetitionResult(int pos, SkiJumper jumperToSet, int series) {
         position = pos;
         skiJumper = jumperToSet;
-        firstJumpResult = null;
-        secondJumpResult = null;
+        jumpResults = new List<JumpResult>();
         points = 0;
+        seriesCount = series;
+
+        for (int index = 0; index < seriesCount; index++) {
+            jumpResults.Add(new JumpResult());
+        }
+    }
+
+    public void SetJumpResult(JumpResult jr, int serie) {
+        jumpResults[serie - 1] = jr;
+
+        points += jr.jumpPoints;
     }
 }
