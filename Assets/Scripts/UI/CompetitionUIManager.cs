ac439d1ac67a5cc8115395420b7d10b33c0946bc
Kamil Bania <kamilbania2@gmail.com>
2020-10-31 01:36:35 +0100

    Competition states, switching views, RunCompetition coroutine, Render UI and handle InputManager

--- Assets/Scripts/UI/CompetitionUIManager.cs
+++ Assets/Scripts/UI/CompetitionUIManager.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Linq;
 public class CompetitionUIManager : UIManager
 {
-    [SerializeField]
-    private Text bestScore;
+    [System.Serializable]
+    public struct View {
+        public string name;
+        public GameObject viewPanel;
 
+        public void SwitchView(View viewToShow) {
+            viewPanel.SetActive(false);
+            viewToShow.viewPanel.SetActive(true);
+        }
+
+        public void Show() {
+            viewPanel.SetActive(true);
+        }
+    } 
+
+    public View[] views;
+
+    private View currentView;
+
+    private SkiJumperSimulator skiJumperSimulator;
+
+    // Competition View UI
     [SerializeField]
     private GameObject jumpResultPanel;
     
@@ -27,32 +47,58 @@ public class CompetitionUIManager : UIManager
     
     [SerializeField]
     public GameObject gameplayPanel;
+    private WindMeterUI windMeter;
+
+    // ************************************
+
+    // Competition Info UI
+
+    public Text hillName;
+    public GameObject resultsScrollPanelContent;
+
+    public GameObject competitionScrollPanelRecordPrefab;
+    private List<GameObject> competitionScrollPanelRecords;
 
     private PlayerController playerController;
+    // *************************************************
+    
+    private bool playerNextMove;
+    private int currentJumper;
+    private int currentSerie;
 
-    private WindMeterUI windMeter;
+    private string competitionState;
+    private const string COMPUTER_NEXT = "COMPUTER_NEXT";
+    private const string PLAYER_NEXT = "PLAYER_NEXT";
+    private const string END_NEXT = "END_NEXT";
 
     public override void Init()
     {
         base.Init();
+        skiJumperSimulator = new SkiJumperSimulator();
+        skiJumperSimulator.SetHill(hill);
+        competitionScrollPanelRecords = new List<GameObject>();
+        currentView = views.Where(view => view.name.Equals("CompetitionInfo")).First();
+        InputManager.SetInputMode(InputManager.COMPETITION_UI);
+        
         HideJumpResultPanel();
         playerController = GameObject.FindObjectOfType<PlayerController>();
-        windMeter = GetComponentInChildren<WindMeterUI>();
+
         helpPanelShow = false;
         playerController.skiJumperStartJumpHandler += HideJumpResultPanel;
-        playerController.skiJumperEndJumpHandler += ShowJumpResultPanel;
-        playerController.skiJumperEndJumpHandler += ShowBestDistance;
-        windMeter.Init();
-    }
+        playerController.skiJumperJumpFinishedHandler += ShowJumpResultPanel;
+        playerController.skiJumperEndJumpHandler += SwitchToInfo;
+        
+        RenderSkiJumpers();
 
-    public void ShowBestDistance() {
-        float bestDistance = playerController.GetBestDistance();
-
-        bestScore.text = "Najlepszy wynik: " + bestDistance.ToString();
+        currentJumper = 0;
+        currentSerie = 1;        
+        
+        NextState();
+        currentView.Show();
     }
 
     public void ShowJumpResultPanel() {
-        PlayerController.JumpResultData jrd = playerController.GetJumpResultData();
+        JumpResult jrd = playerController.GetJumpResultData();
         distanceValue.text = jrd.jumpDistance.ToString();
         resultValue.text = jrd.jumpPoints.ToString();
         
@@ -80,6 +126,143 @@ public class CompetitionUIManager : UIManager
         gameplayPanel.SetActive(!helpPanelShow);
     }
 
+    public void SwitchToCompetition() {
+        View competitionView = views.Where(view => view.name.Equals("CompetitionView")).First();
+        currentView.SwitchView(competitionView);
+        windMeter = GetComponentInChildren<WindMeterUI>();
+        windMeter.Init();
+        InputManager.SetInputMode(InputManager.SKI_JUMPER);
+        playerController.ResetSkiJumper();
+    }
+
+    public void SwitchToInfo() {
+        View competitionInfo = views.Where(view => view.name.Equals("CompetitionInfo")).First();
+        currentView.SwitchView(competitionInfo);
+        currentView = competitionInfo;
+        InputManager.SetInputMode(InputManager.COMPETITION_UI);
+        RenderPlayerResult();
+        NextState();
+    }
+
+    public void RunSimulation() {
+        if (competitionState == PLAYER_NEXT) {
+            SwitchToCompetition();
+            return;
+        }
+        else if (competitionState == COMPUTER_NEXT) {
+            StartCoroutine(RunCompetition());
+        }
+        else if (competitionState == END_NEXT) {
+            // zakoncz konkurs
+            SceneManager.LoadScene("MainMenu");
+            return;
+        }
+    }
+
+    public IEnumerator RunCompetition() {
+        while(true) {
+            if (competitionState == PLAYER_NEXT || competitionState == END_NEXT) {
+                yield break;
+            }
+
+            // symulacja skoku komputera
+            JumpResult computerResult = skiJumperSimulator.SimulateJump();
+            CompetitionResultRecord crr = competitionScrollPanelRecords[currentJumper].GetComponent<CompetitionResultRecord>();
+            crr.competitionResult.SetJumpResult(computerResult, currentSerie);
+            Debug.Log("Odleglosc komputera: " + computerResult.jumpDistance);
+            crr.resultPoints.text = crr.competitionResult.points.ToString() + "pkt";
+
+            if (currentSerie == 1) {
+                crr.firstJump.text = crr.competitionResult.jumpResults[0].jumpDistance.ToString() + "m";
+            }
+            else {
+                crr.secondJump.text = crr.competitionResult.jumpResults[1].jumpDistance.ToString()+ "m";
+            }
+
+            currentJumper++;
+
+            yield return new WaitForSeconds(0.5f);
+            NextState();
+        }   
+    }
+
+    private void NextState() {
+        Text playButtonText = currentView.viewPanel.GetComponentInChildren<Button>().gameObject.GetComponentInChildren<Text>();
+        
+        if (currentJumper == competitionScrollPanelRecords.Count) {
+            competitionState = END_NEXT;
+            playButtonText.text = "Zakończ";
+            return;
+        }
+
+        SetIsPlayerNext();
+
+        if (playerNextMove) {
+            competitionState = PLAYER_NEXT;
+            playButtonText.text = "Skacz";
+        }
+        else {
+            competitionState = COMPUTER_NEXT;
+            playButtonText.text = "Kontynuuj";
+        }
+    }
+
+    public void SetIsPlayerNext() {
+        if (competitionScrollPanelRecords.ElementAt(currentJumper).GetComponent<CompetitionResultRecord>().competitionResult.skiJumper.isComputer) {
+            playerNextMove = false;
+        }
+        else {
+            playerNextMove = true;
+        }
+        Debug.Log("PlayerNextMove: " + playerNextMove);
+    }
+
+    public void RenderSkiJumpers() {
+        List<SkiJumper> skiJumpers = SkiJumperDatabase.LoadSkiJumpers();
+        int position = 1;
+        GameObject record;
+        CompetitionResult competitionResult;
+        CompetitionResultRecord competitionResultRecord;
+
+
+        foreach (SkiJumper sj in skiJumpers) {
+            record = Instantiate(competitionScrollPanelRecordPrefab, resultsScrollPanelContent.transform);
+            competitionResult = new CompetitionResult(position, sj, 2);
+            competitionResultRecord = record.GetComponent<CompetitionResultRecord>();
+            competitionResultRecord.SetCompetitionResult(competitionResult); 
+            competitionResultRecord.Render();
+
+            competitionScrollPanelRecords.Add(record);
+            position++;
+        }
+
+        GameObject playerRecord = Instantiate(competitionScrollPanelRecordPrefab, resultsScrollPanelContent.transform);
+        SkiJumper player = new SkiJumper("Gracz Graczowy", "Polska", false);
+        CompetitionResult playerCr = new CompetitionResult(position, player, 2);
+        CompetitionResultRecord playerCrpr = playerRecord.GetComponent<CompetitionResultRecord>();
+        playerCrpr.SetCompetitionResult(playerCr);
+        playerCrpr.Render();
+
+        competitionScrollPanelRecords.Add(playerRecord);
+    }
+
+    public void RenderPlayerResult() {
+        JumpResult jr = playerController.GetJumpResultData();
+        CompetitionResultRecord crr = competitionScrollPanelRecords[currentJumper].GetComponent<CompetitionResultRecord>();
+        
+        crr.competitionResult.SetJumpResult(jr, currentSerie);
+        crr.resultPoints.text = crr.competitionResult.points.ToString() + "pkt";
+
+        if (currentSerie == 1) {
+            crr.firstJump.text = crr.competitionResult.jumpResults[0].jumpDistance.ToString() + "m";
+        }
+        else {
+            crr.secondJump.text = crr.competitionResult.jumpResults[1].jumpDistance.ToString()+ "m";
+        }
+
+        currentJumper++;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -89,10 +272,13 @@ public class CompetitionUIManager : UIManager
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.H)) {
-            ToggleHelpPanel();
+        if (InputManager.currentInputMode == InputManager.SKI_JUMPER) {
+            if (Input.GetKeyDown(KeyCode.H)) {
+                ToggleHelpPanel();
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Escape)) {
+        
+        if (Input.GetKeyDown(KeyCode.Escape)) {
             SceneManager.LoadScene("MainMenu");
         }
     }
