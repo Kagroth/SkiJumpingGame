d0b8952da7ee8ed6851ad5a7690616f44f9dc04e
Kamil Bania <kamilbania2@gmail.com>
2020-11-08 02:16:29 +0100

    Country strings changed to Country consts

--- Assets/Scripts/SkiJumpersData/SkiJumperDatabase.cs
+++ Assets/Scripts/SkiJumpersData/SkiJumperDatabase.cs
@@ -31,111 +31,111 @@ public class SkiJumperDatabase
 
     private static void GenerateSkiJumpersList(List<SkiJumper> skiJumpersList) {
         // Austria
-        skiJumpersList.Add(new SkiJumper("Richard Kraus", "Austria", true));
-        skiJumpersList.Add(new SkiJumper("Barnabas Von Grimmelshausen", "Austria", true));
-        skiJumpersList.Add(new SkiJumper("Benjamin Zellweger", "Austria", true));
-        skiJumpersList.Add(new SkiJumper("Hartwig Laurenz", "Austria", true));
-        skiJumpersList.Add(new SkiJumper("Eckart Gros", "Austria", true));
-        skiJumpersList.Add(new SkiJumper("Gert Bernhard", "Austria", true));
-        skiJumpersList.Add(new SkiJumper("Gotthold Grosser", "Austria", true));
-        skiJumpersList.Add(new SkiJumper("Dominik Mein", "Austria", true));
+        skiJumpersList.Add(new SkiJumper("Richard Kraus", Country.AUSTRIA, true));
+        skiJumpersList.Add(new SkiJumper("Barnabas Von Grimmelshausen", Country.AUSTRIA, true));
+        skiJumpersList.Add(new SkiJumper("Benjamin Zellweger", Country.AUSTRIA, true));
+        skiJumpersList.Add(new SkiJumper("Hartwig Laurenz", Country.AUSTRIA, true));
+        skiJumpersList.Add(new SkiJumper("Eckart Gros", Country.AUSTRIA, true));
+        skiJumpersList.Add(new SkiJumper("Gert Bernhard", Country.AUSTRIA, true));
+        skiJumpersList.Add(new SkiJumper("Gotthold Grosser", Country.AUSTRIA, true));
+        skiJumpersList.Add(new SkiJumper("Dominik Mein", Country.AUSTRIA, true));
 
         // Bułgaria
-        skiJumpersList.Add(new SkiJumper("Angel Chavdarov", "Bulgaria", true));
-        skiJumpersList.Add(new SkiJumper("Rayko Simeonov", "Bulgaria", true));
+        skiJumpersList.Add(new SkiJumper("Angel Chavdarov", Country.BULGARIA, true));
+        skiJumpersList.Add(new SkiJumper("Rayko Simeonov", Country.BULGARIA, true));
 
         // Czechy
-        skiJumpersList.Add(new SkiJumper("Kornel Kader", "Czechy", true));
-        skiJumpersList.Add(new SkiJumper("Julius Klimek", "Czechy", true));
-        skiJumpersList.Add(new SkiJumper("Vavrinec Laska", "Czechy", true));
-        skiJumpersList.Add(new SkiJumper("Vitezslav Zitnik", "Czechy", true));
-        skiJumpersList.Add(new SkiJumper("Vlasitimir Kozel", "Czechy", true));
+        skiJumpersList.Add(new SkiJumper("Kornel Kader", Country.CZECH_REPUBLIC, true));
+        skiJumpersList.Add(new SkiJumper("Julius Klimek", Country.CZECH_REPUBLIC, true));
+        skiJumpersList.Add(new SkiJumper("Vavrinec Laska", Country.CZECH_REPUBLIC, true));
+        skiJumpersList.Add(new SkiJumper("Vitezslav Zitnik", Country.CZECH_REPUBLIC, true));
+        skiJumpersList.Add(new SkiJumper("Vlasitimir Kozel", Country.CZECH_REPUBLIC, true));
 
         // Estonia
-        skiJumpersList.Add(new SkiJumper("Meelis Tamm", "Estonia", true));
-        skiJumpersList.Add(new SkiJumper("Andres Kukk", "Estonia", true));
+        skiJumpersList.Add(new SkiJumper("Meelis Tamm", Country.ESTONIA, true));
+        skiJumpersList.Add(new SkiJumper("Andres Kukk", Country.ESTONIA, true));
 
         // Finlandia
-        skiJumpersList.Add(new SkiJumper("Viljami Ruotsalainen", "Finlandia", true));
-        skiJumpersList.Add(new SkiJumper("Jalmari Linna", "Finlandia", true));
-        skiJumpersList.Add(new SkiJumper("Aatu Virtanen", "Finlandia", true));
-        skiJumpersList.Add(new SkiJumper("Teuvo Jarvinen", "Finlandia", true));
-        skiJumpersList.Add(new SkiJumper("Manu Karppinen", "Finlandia", true));
+        skiJumpersList.Add(new SkiJumper("Viljami Ruotsalainen", Country.FINLAND, true));
+        skiJumpersList.Add(new SkiJumper("Jalmari Linna", Country.FINLAND, true));
+        skiJumpersList.Add(new SkiJumper("Aatu Virtanen", Country.FINLAND, true));
+        skiJumpersList.Add(new SkiJumper("Teuvo Jarvinen", Country.FINLAND, true));
+        skiJumpersList.Add(new SkiJumper("Manu Karppinen", Country.FINLAND, true));
 
         // Japonia
-        skiJumpersList.Add(new SkiJumper("Osamu Tamura", "Japonia", true));
-        skiJumpersList.Add(new SkiJumper("Ryoichi Tamura", "Finlandia", true));
-        skiJumpersList.Add(new SkiJumper("Kazuo Sugiyama", "Finlandia", true));
-        skiJumpersList.Add(new SkiJumper("Kyou Yamauchi", "Finlandia", true));
-        skiJumpersList.Add(new SkiJumper("Hayato Tanaka", "Finlandia", true));
-        skiJumpersList.Add(new SkiJumper("Takahiro Minami", "Finlandia", true));
+        skiJumpersList.Add(new SkiJumper("Osamu Tamura", Country.JAPAN, true));
+        skiJumpersList.Add(new SkiJumper("Ryoichi Tamura", Country.JAPAN, true));
+        skiJumpersList.Add(new SkiJumper("Kazuo Sugiyama", Country.JAPAN, true));
+        skiJumpersList.Add(new SkiJumper("Kyou Yamauchi", Country.JAPAN, true));
+        skiJumpersList.Add(new SkiJumper("Hayato Tanaka", Country.JAPAN, true));
+        skiJumpersList.Add(new SkiJumper("Takahiro Minami", Country.JAPAN, true));
 
         // Kanada
-        skiJumpersList.Add(new SkiJumper("Cayden Cairns", "Kanada", true));
-        skiJumpersList.Add(new SkiJumper("Emerson Jones", "Kanada", true));
+        skiJumpersList.Add(new SkiJumper("Cayden Cairns", Country.CANADA, true));
+        skiJumpersList.Add(new SkiJumper("Emerson Jones", Country.CANADA, true));
 
         // Kazachstan
-        skiJumpersList.Add(new SkiJumper("Azat Bulat", "Kazachstan", true));
-        skiJumpersList.Add(new SkiJumper("Rustam Erasyl", "Kazachstan", true));
+        skiJumpersList.Add(new SkiJumper("Azat Bulat", Country.KAZAKHSTAN, true));
+        skiJumpersList.Add(new SkiJumper("Rustam Erasyl", Country.KAZAKHSTAN, true));
 
         // Niemcy
-        skiJumpersList.Add(new SkiJumper("Matthias Gunther", "Niemcy", true));
-        skiJumpersList.Add(new SkiJumper("Sebastian Armbruster", "Niemcy", true));
-        skiJumpersList.Add(new SkiJumper("Edmund Seidel", "Niemcy", true));
-        skiJumpersList.Add(new SkiJumper("Ferdi Riese", "Niemcy", true));
-        skiJumpersList.Add(new SkiJumper("Tim Schuttmann", "Niemcy", true));
-        skiJumpersList.Add(new SkiJumper("Aaron Loewe", "Niemcy", true));
-        skiJumpersList.Add(new SkiJumper("August Geier", "Niemcy", true));
-        skiJumpersList.Add(new SkiJumper("Wendelin Schlosser", "Niemcy", true));
+        skiJumpersList.Add(new SkiJumper("Matthias Gunther", Country.GERMANY, true));
+        skiJumpersList.Add(new SkiJumper("Sebastian Armbruster", Country.GERMANY, true));
+        skiJumpersList.Add(new SkiJumper("Edmund Seidel", Country.GERMANY, true));
+        skiJumpersList.Add(new SkiJumper("Ferdi Riese", Country.GERMANY, true));
+        skiJumpersList.Add(new SkiJumper("Tim Schuttmann", Country.GERMANY, true));
+        skiJumpersList.Add(new SkiJumper("Aaron Loewe", Country.GERMANY, true));
+        skiJumpersList.Add(new SkiJumper("August Geier", Country.GERMANY, true));
+        skiJumpersList.Add(new SkiJumper("Wendelin Schlosser", Country.GERMANY, true));
 
         // Norwegia
-        skiJumpersList.Add(new SkiJumper("Mats Halvorsen", "Norwegia", true));
-        skiJumpersList.Add(new SkiJumper("Alexander Iversen", "Norwegia", true));
-        skiJumpersList.Add(new SkiJumper("Fredrik Andersen", "Norwegia", true));
-        skiJumpersList.Add(new SkiJumper("Halle Dahl", "Norwegia", true));
-        skiJumpersList.Add(new SkiJumper("Torger Hansen", "Norwegia", true));
-        skiJumpersList.Add(new SkiJumper("Bjarte Stenberg", "Norwegia", true));
-        skiJumpersList.Add(new SkiJumper("Ebbe Ernst Antonsen", "Norwegia", true));
-        skiJumpersList.Add(new SkiJumper("Andor Torgils Vang", "Norwegia", true));
+        skiJumpersList.Add(new SkiJumper("Mats Halvorsen", Country.NORWAY, true));
+        skiJumpersList.Add(new SkiJumper("Alexander Iversen", Country.NORWAY, true));
+        skiJumpersList.Add(new SkiJumper("Fredrik Andersen", Country.NORWAY, true));
+        skiJumpersList.Add(new SkiJumper("Halle Dahl", Country.NORWAY, true));
+        skiJumpersList.Add(new SkiJumper("Torger Hansen", Country.NORWAY, true));
+        skiJumpersList.Add(new SkiJumper("Bjarte Stenberg", Country.NORWAY, true));
+        skiJumpersList.Add(new SkiJumper("Ebbe Ernst Antonsen", Country.NORWAY, true));
+        skiJumpersList.Add(new SkiJumper("Andor Torgils Vang", Country.NORWAY, true));
 
         // Polska
-        skiJumpersList.Add(new SkiJumper("Przemysław Popławski", "Polska", true));
-        skiJumpersList.Add(new SkiJumper("Tymoteusz Kowalski", "Polska", true));
-        skiJumpersList.Add(new SkiJumper("Jakub Wasilewski", "Polska", true));
-        skiJumpersList.Add(new SkiJumper("Kamil Zima", "Polska", true));
-        skiJumpersList.Add(new SkiJumper("Adam Czajka", "Polska", true));
-        skiJumpersList.Add(new SkiJumper("Kornel Pokorny", "Polska", true));
-        skiJumpersList.Add(new SkiJumper("Krystian Ząbek", "Polska", true));
+        skiJumpersList.Add(new SkiJumper("Przemysław Popławski", Country.POLAND, true));
+        skiJumpersList.Add(new SkiJumper("Tymoteusz Kowalski", Country.POLAND, true));
+        skiJumpersList.Add(new SkiJumper("Jakub Wasilewski", Country.POLAND, true));
+        skiJumpersList.Add(new SkiJumper("Kamil Zima", Country.POLAND, true));
+        skiJumpersList.Add(new SkiJumper("Adam Czajka", Country.POLAND, true));
+        skiJumpersList.Add(new SkiJumper("Kornel Pokorny", Country.POLAND, true));
+        skiJumpersList.Add(new SkiJumper("Krystian Ząbek", Country.POLAND, true));
 
         // Rosja
-        skiJumpersList.Add(new SkiJumper("Evgeni Orlov", "Rosja", true));
-        skiJumpersList.Add(new SkiJumper("Dmitrij Krupin", "Rosja", true));
-        skiJumpersList.Add(new SkiJumper("Yury Naumov", "Rosja", true));
-        skiJumpersList.Add(new SkiJumper("Vladimir Viktorov", "Rosja", true));
+        skiJumpersList.Add(new SkiJumper("Evgeni Orlov", Country.RUSSIA, true));
+        skiJumpersList.Add(new SkiJumper("Dmitrij Krupin", Country.RUSSIA, true));
+        skiJumpersList.Add(new SkiJumper("Yury Naumov", Country.RUSSIA, true));
+        skiJumpersList.Add(new SkiJumper("Vladimir Viktorov", Country.RUSSIA, true));
 
         // Słowenia
-        skiJumpersList.Add(new SkiJumper("Jernej Gasper", "Słowenia", true));
-        skiJumpersList.Add(new SkiJumper("Bojan Plesko", "Słowenia", true));
-        skiJumpersList.Add(new SkiJumper("Tijan Strnad", "Słowenia", true));
-        skiJumpersList.Add(new SkiJumper("Grega Korosec", "Słowenia", true));
-        skiJumpersList.Add(new SkiJumper("Vinko Zupancic", "Słowenia", true));
-        skiJumpersList.Add(new SkiJumper("Drago Babic", "Słowenia", true));
-        skiJumpersList.Add(new SkiJumper("Borut Babic", "Słowenia", true));
+        skiJumpersList.Add(new SkiJumper("Jernej Gasper", Country.SLOVENIA, true));
+        skiJumpersList.Add(new SkiJumper("Bojan Plesko", Country.SLOVENIA, true));
+        skiJumpersList.Add(new SkiJumper("Tijan Strnad", Country.SLOVENIA, true));
+        skiJumpersList.Add(new SkiJumper("Grega Korosec", Country.SLOVENIA, true));
+        skiJumpersList.Add(new SkiJumper("Vinko Zupancic", Country.SLOVENIA, true));
+        skiJumpersList.Add(new SkiJumper("Drago Babic", Country.SLOVENIA, true));
+        skiJumpersList.Add(new SkiJumper("Borut Babic", Country.SLOVENIA, true));
 
         // Szwajcaria
-        skiJumpersList.Add(new SkiJumper("Gido Kernen", "Szwajcaria", true));
-        skiJumpersList.Add(new SkiJumper("Niklas Zahnd", "Szwajcaria", true));
-        skiJumpersList.Add(new SkiJumper("Jost Held", "Szwajcaria", true));
-        skiJumpersList.Add(new SkiJumper("Leonardo Habrerli", "Szwajcaria", true));
+        skiJumpersList.Add(new SkiJumper("Gido Kernen", Country.SWITZERLAND, true));
+        skiJumpersList.Add(new SkiJumper("Niklas Zahnd", Country.SWITZERLAND, true));
+        skiJumpersList.Add(new SkiJumper("Jost Held", Country.SWITZERLAND, true));
+        skiJumpersList.Add(new SkiJumper("Leonardo Habrerli", Country.SWITZERLAND, true));
 
         // Stany Zjednoczone
-        skiJumpersList.Add(new SkiJumper("Royston Tennison", "Stany Zjednoczone", true));
-        skiJumpersList.Add(new SkiJumper("Cedric Bateson", "Stany Zjednoczone", true));
+        skiJumpersList.Add(new SkiJumper("Royston Tennison", Country.USA, true));
+        skiJumpersList.Add(new SkiJumper("Cedric Bateson", Country.USA, true));
 
         // Francja
-        skiJumpersList.Add(new SkiJumper("Lothaire Lyon", "Francja", true));
-        skiJumpersList.Add(new SkiJumper("Emeric Thayer", "Francja", true));
-        skiJumpersList.Add(new SkiJumper("Jean-Francois Pierre", "Francja", true));
-        skiJumpersList.Add(new SkiJumper("Matheo Gerard", "Francja", true));
+        skiJumpersList.Add(new SkiJumper("Lothaire Lyon", Country.FRANCE, true));
+        skiJumpersList.Add(new SkiJumper("Emeric Thayer", Country.FRANCE, true));
+        skiJumpersList.Add(new SkiJumper("Jean-Francois Pierre", Country.FRANCE, true));
+        skiJumpersList.Add(new SkiJumper("Matheo Gerard", Country.FRANCE, true));
     }
 }
