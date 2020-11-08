bd4eb20681fc7c9a373c16e2d3d9485445070e40
Kamil Bania <kamilbania2@gmail.com>
2020-11-08 02:17:17 +0100

    Added bibNumer and countryFlag properties. countryFlag loaded from resources.

--- Assets/Scripts/UI/CompetitionResultRecord.cs
+++ Assets/Scripts/UI/CompetitionResultRecord.cs
@@ -7,6 +7,8 @@ public class CompetitionResultRecord : MonoBehaviour
 {
     public bool faded = false;
     public Text position;
+    public Text bibNumer;
+    public Image countryFlag;
     public Text skiJumperName;
     public Text firstJump;
     public Text secondJump;
@@ -15,6 +17,9 @@ public class CompetitionResultRecord : MonoBehaviour
 
     public void SetCompetitionResult(CompetitionResult cr) {
         competitionResult = cr;
+        bibNumer.text = cr.bib.ToString();
+        Debug.Log("Kraj: " + cr.skiJumper.country);
+        countryFlag.sprite = Resources.Load<Sprite>("Sprites/Country Flags/" + cr.skiJumper.country);
     }
 
     public void SetFade(bool fade) {
@@ -23,6 +28,7 @@ public class CompetitionResultRecord : MonoBehaviour
 
     public void SetColor(Color newColor) {
         position.color      = newColor;
+        bibNumer.color      = newColor;
         skiJumperName.color = newColor;
         firstJump.color     = newColor;
         secondJump.color    = newColor;
