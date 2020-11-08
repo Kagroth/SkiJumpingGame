e90674df4a775ce59a388308b82f5ef80d35cd6c
Kamil Bania <kamilbania2@gmail.com>
2020-11-08 02:13:34 +0100

    Country class with sprites of countries flags names

---
+++ Assets/Scripts/UI/Country.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Country
+{
+    public const string AUSTRIA = "aut";
+    public const string BULGARIA = "bgr";
+    public const string CZECH_REPUBLIC = "cze";
+    public const string ESTONIA = "est";
+    public const string FINLAND = "fin";
+    public const string JAPAN = "jpn";
+    public const string CANADA = "can";
+    public const string KAZAKHSTAN = "kaz";
+    public const string GERMANY = "deu";
+    public const string NORWAY = "nor";
+    public const string POLAND = "pol";
+    public const string RUSSIA = "rus";
+    public const string SLOVENIA = "svn";
+    public const string SWITZERLAND = "che";
+    public const string USA = "usa";
+    public const string FRANCE = "fra";
+    public const string ITALY = "ita";
+
+
+}
