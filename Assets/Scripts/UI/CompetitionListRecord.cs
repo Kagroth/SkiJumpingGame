7bcaed0c921567757e4f79a4ede0fa684b9aed1d
Kamil Bania <kamilbania2@gmail.com>
2020-11-09 21:44:34 +0100

    WorldCup competition list record

---
+++ Assets/Scripts/UI/CompetitionListRecord.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CompetitionListRecord : MonoBehaviour
+{
+    public Text position;
+    public Text hillName;
+    public Text competitionType;
+
+    private bool completed;
+
+    public void SetData(int pos, string newHillName, string newType) {
+        position.text = pos.ToString();
+        hillName.text = newHillName;
+        competitionType.text = newType;
+        completed = false;
+    }
+
+    public void Complete() {
+        completed = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
