261896700d7ed57f600450f8c83759b10e16c820
Kamil Bania <kamilbania2@gmail.com>
2020-12-16 19:46:45 +0100

    FourHillSkiJumperResult class created

---
+++ Assets/Scripts/WorldCup/FourHillSkiJumperResult.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourHillSkiJumperResult
+{
+    public SkiJumper skiJumper;
+    public float points;
+
+    public FourHillSkiJumperResult(SkiJumper jumper) {
+        skiJumper = jumper;
+        points = 0;
+    }
+
+    public static int Compare(FourHillSkiJumperResult result1, FourHillSkiJumperResult result2) {
+        if (result1.points > result2.points) {
+            return -1;
+        }
+        else if (result1.points == result2.points) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+}
