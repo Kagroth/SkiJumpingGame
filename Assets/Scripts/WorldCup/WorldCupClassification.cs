6ad0a213f93b498c5d4f4ebe41c5c3e779e89b6e
Kamil Bania <kamilbania2@gmail.com>
2020-12-16 19:48:51 +0100

    WorldCupCLassification class created with constructor definition

---
+++ Assets/Scripts/WorldCup/WorldCupClassification.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// przepisanie na wzorzec builder
+public class WorldCupClassification
+{
+    public List<SkiJumper> worldCupParticipants;
+    public List<WorldCupSkiJumperResult> worldCupList;
+    public List<WorldCupSkiJumperResult> worldCupFlyingList;
+    public List<FourHillSkiJumperResult> fourHillTournamentList;
+
+    public WorldCupClassification() {
+        worldCupList = new List<WorldCupSkiJumperResult>();
+        worldCupFlyingList = new List<WorldCupSkiJumperResult>();
+        fourHillTournamentList = new List<FourHillSkiJumperResult>();
+
+        foreach (SkiJumper skiJumper in worldCupParticipants) {
+            WorldCupSkiJumperResult normalResult = new WorldCupSkiJumperResult(skiJumper);
+            WorldCupSkiJumperResult flyingResult = new WorldCupSkiJumperResult(skiJumper);
+            FourHillSkiJumperResult fourHillResult = new FourHillSkiJumperResult(skiJumper);
+
+            worldCupList.Add(normalResult);
+            worldCupFlyingList.Add(flyingResult);
+            fourHillTournamentList.Add(fourHillResult);
+        }
+    }
+
+    public WorldCupClassification(List<SkiJumper> participantsToSet) : this() {
+        worldCupParticipants = participantsToSet;
+    }
+}
